49d0938096c09cbbb9973bc645c2c64a0a947c51
Eduardo Burgos Humbo <edu_94_bhf@hotmail.com>
2022-10-30 00:15:17 -0500

    logrado

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import  img  from "./img.json";
 
 function App() {
   const [time, setTime] = useState({});
@@ -22,87 +23,119 @@ function App() {
   const onChnge = () => {
     setChange(!change);
   };
-  let fundTime = time.weather?.[0].icon;
   let linkFund = "";
 
-  switch (fundTime) {
-    case "01d":
-      linkFund =
-        "https://images.pexels.com/photos/4671463/pexels-photo-4671463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "01n":
-      linkFund =
-        "https://images.pexels.com/photos/13891178/pexels-photo-13891178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "02d":
-      linkFund =
-        "https://images.pexels.com/photos/5069533/pexels-photo-5069533.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "02n":
-      linkFund =
-        "https://images.pexels.com/photos/13511276/pexels-photo-13511276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "04n":
-      linkFund =
-        "https://images.pexels.com/photos/12262726/pexels-photo-12262726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "03n":
-      linkFund =
-        "https://images.pexels.com/photos/12903460/pexels-photo-12903460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "03d":
-      linkFund =
-        "https://images.pexels.com/photos/11513041/pexels-photo-11513041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "04d":
-      linkFund =
-        "https://images.pexels.com/photos/12907149/pexels-photo-12907149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "09d":
-      linkFund =
-        "https://images.pexels.com/photos/2144326/pexels-photo-2144326.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "09n":
-      linkFund =
-        "https://images.pexels.com/photos/2618980/pexels-photo-2618980.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "10d":
-      linkFund =
-        "https://images.pexels.com/photos/1755702/pexels-photo-1755702.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "10n":
-      linkFund =
-        "https://images.pexels.com/photos/11408850/pexels-photo-11408850.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "11d":
-      linkFund =
-        "https://images.pexels.com/photos/1446076/pexels-photo-1446076.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "11n":
-      linkFund =
-        "https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "13d":
-      linkFund =
-        "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "13n":
-      linkFund =
-        "https://images.pexels.com/photos/2422497/pexels-photo-2422497.jpeg?auto=compress&cs=tinysrgb&w=800";
-      break;
-    case "50d":
-      linkFund =
-        "https://images.pexels.com/photos/7919/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-    case "50n":
-      linkFund =
-        "https://images.pexels.com/photos/3299386/pexels-photo-3299386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-      break;
-      default: 
-      linkFund =
-        "https://images.pexels.com/photos/3299386/pexels-photo-3299386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
-     
+  // switch (fundTime) {
+  //   case "01d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/4671463/pexels-photo-4671463.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "01n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/13891178/pexels-photo-13891178.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "02d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/5069533/pexels-photo-5069533.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "02n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/13511276/pexels-photo-13511276.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "04n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/12262726/pexels-photo-12262726.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "03n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/12903460/pexels-photo-12903460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "03d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/11513041/pexels-photo-11513041.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "04d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/12907149/pexels-photo-12907149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "09d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/2144326/pexels-photo-2144326.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "09n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/2618980/pexels-photo-2618980.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "10d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/1755702/pexels-photo-1755702.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "10n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/11408850/pexels-photo-11408850.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "11d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/1446076/pexels-photo-1446076.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "11n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/1162251/pexels-photo-1162251.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "13d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/869258/pexels-photo-869258.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "13n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/2422497/pexels-photo-2422497.jpeg?auto=compress&cs=tinysrgb&w=800";
+  //     break;
+  //   case "50d":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/7919/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //   case "50n":
+  //     linkFund =
+  //       "https://images.pexels.com/photos/3299386/pexels-photo-3299386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  //     break;
+  //     default:
+  //     linkFund =
+  //       "https://images.pexels.com/photos/3299386/pexels-photo-3299386.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+  // }
+  for (let i = 10; i <= 100; i += 10) {
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 10) {
+      linkFund = img.img1;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 20) {
+      linkFund = img.img2;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 30) {
+      linkFund = img.img3;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 40) {
+      linkFund = img.img4;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 50) {
+      linkFund = img.img5;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 60) {
+      linkFund = img.img6;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 70) {
+      linkFund = img.img7;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 80) {
+      linkFund = img.img8;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 90) {
+      linkFund = img.img9;
+    }
+    if (`${(time.main?.temp - 273.15).toFixed(1)}` < 100) {
+      linkFund = img.img10;
+    }
   }
+
   document.body.style = `background-image: url(${linkFund})`;
 
   return (
@@ -119,7 +152,7 @@ function App() {
             alt=""
           />
           <p>
-            <i class="fa-solid fa-temperature-quarter"></i>{" "}
+            <i className="fa-solid fa-temperature-quarter"></i>{" "}
             {change
               ? `${(time.main?.temp - 273.15).toFixed(1)}°c`
               : `${(((time.main?.temp - 273.15) * 9) / 5 + 32).toFixed(1)}°f`}
@@ -128,18 +161,18 @@ function App() {
 
         <ul>
           <li>
-            <i class="fa-solid fa-wind"></i> <strong>wind speed :</strong>{" "}
+            <i className="fa-solid fa-wind"></i> <strong>wind speed :</strong>{" "}
             {time.wind?.speed} m/s
           </li>
           <li>
-            <i class="fa-solid fa-cloud"></i> <strong>clouds :</strong>{" "}
+            <i className="fa-solid fa-cloud"></i> <strong>clouds :</strong>{" "}
             {time.clouds?.all} %
           </li>
           <li>
-            <i class="fa-solid fa-feather"></i> <strong>pressure :</strong>{" "}
+            <i className="fa-solid fa-feather"></i> <strong>pressure :</strong>{" "}
             {time.main?.pressure} mb
           </li>
-        </ul> 
+        </ul>
       </div>
       <button onClick={onChnge}>ºC/ºFh</button>
     </div>
